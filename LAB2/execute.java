a6dc196f86b5f0c708e89d8fb587431da9c1d864
Vas <148073365+vas3105@users.noreply.github.com>
2024-08-20 18:33:18 +0530

    Create execute.java

---
+++ LAB2/execute.java
@@ -0,0 +1,13 @@
+class execute
+{
+public static void main(String args[])
+{
+int x=10;
+double y=x;
+System.out.println(y);
+double d=10.5;
+//int a = d;
+//System.out.println(a);
+int s= (int)d;
+System.out.println(s);
+}}
