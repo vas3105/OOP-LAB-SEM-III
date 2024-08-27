fe9588afa3ad53d9dd4c9015ff1bd468a085a749
Vasavi A S <148073365+vas3105@users.noreply.github.com>
2024-08-27 18:55:56 +0530

    Create comb.java

---
+++ lab3/comb.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class comb {
+    
+public static void main(String[] args) {
+     Scanner sc= new Scanner (System.in);
+     int n;
+     n=sc.nextInt();
+     int a[] =new int[4];
+     for(int i=0;i<4;i++)
+     {
+        int rem=n%10;
+        a[i]=rem;
+        n=n/10;
+     }
+     for(int i=0;i<4;i++)
+     {
+        
+     }
+}
+}
