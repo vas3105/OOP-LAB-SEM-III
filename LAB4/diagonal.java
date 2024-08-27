4567197af8b0a0ee564d78809b5ad639d6bd9156
Vasavi A S <148073365+vas3105@users.noreply.github.com>
2024-08-27 19:00:31 +0530

    Create diagonal.java

---
+++ LAB4/diagonal.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+public class diagonal 
+{
+    public static void main(String[] args) 
+    {
+        int i,j,n,sum=0;
+      Scanner sc= new Scanner(System.in);
+      System.out.println("enter the number of rows");
+      n=sc.nextInt();
+      int a[][]=new int[n][n];
+      System.out.println("enter the elements into the array for a square matrix");
+      for(i=0;i<n;i++)
+      {
+        for(j=0;j<n;j++)
+        {
+            a[i][j]=sc.nextInt();
+        }
+      }
+      System.out.println(" the  diagonal elements are ");
+      for(i=0;i<n;i++)
+      {
+        for(j=0;j<n;j++)
+        {
+           if(i==j)
+           {
+            System.out.println(" "+a[i][j]);
+            sum=a[i][j]+sum;
+           }
+        }
+      }
+      System.out.println(" the sum of diagonal elements is "+sum);
+    }
+    
+}
