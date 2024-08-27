a1734ec60a3c9ee808412306109768affd003fab
Vasavi A S <148073365+vas3105@users.noreply.github.com>
2024-08-27 18:56:47 +0530

    Create design2.java

---
+++ lab3/design2.java
@@ -0,0 +1,13 @@
+public class design2 {
+    public static void main(String args[])
+    {
+        int arr[]={1,2,3,4,5};
+        for(int i:arr) 
+        { 
+       for(int j=0;j<i;j++)
+        {
+            System.out.print(i+"\t");
+        }System.out.println(" ");
+    }
+    }
+}
